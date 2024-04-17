f766b805a80dbf462dd2b45e1b7f6051e054c0c3
LucasMSunn <167314685+LucasMSunn@users.noreply.github.com>
2024-04-17 14:42:04 +0200

    Update dpu_inference.py

--- Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_inference.py
+++ Quantizing-Compiling-YOLOv3-Pytorch-with-DPU-Inference/DPU inference/dpu_inference.py
@@ -111,8 +111,8 @@ def runYolo(dpu_runner_tfYolo, image, config, image_path):
                 cv2.putText(im, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
         # # Save generated image with detections
-         output_path = 'prediction.jpg'
-         cv2.imwrite(output_path, im)
+        output_path = 'prediction.jpg'
+        cv2.imwrite(output_path, im)
 
         # Display image
         #cv2.imshow("Prediction", im)
